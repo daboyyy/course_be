b44ccdc08ba621faed31c3dc43f7026d29357c9b
jiniusz <dev.jiniusz@gmail.com>
2023-09-13 16:21:22 +0700

    fix: try to fix heroku build

---
+++ app.js
@@ -0,0 +1,21 @@
+const cors = require("cors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+// Internal
+const { verifyAccessToken } = require("./middleware/verifyAccessToken");
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+const app = express();
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+// Routes
+app.use("/auth", require("./routes/auth"));
+app.use("/customerInfo", verifyAccessToken, require("./routes/customerInfo"));
+app.use("/course", verifyAccessToken, require("./routes/course"));
--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-const cors = require("cors");
-const express = require("express");
-const cookieParser = require("cookie-parser");
+import app from "./app.js";
+import dotenv from "dotenv";
 
-// Internal
-const { verifyAccessToken } = require("./middleware/verifyAccessToken");
+dotenv.config();
 
-require("dotenv").config();
-
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-
-const app = express();
-app.use(express.json());
-app.use(cors(corsOptions));
-app.use(cookieParser());
-
-// Routes
-app.use("/auth", require("./routes/auth"));
-app.use("/customerInfo", verifyAccessToken, require("./routes/customerInfo"));
-app.use("/course", verifyAccessToken, require("./routes/course"));
-
-// Listen
-app.listen(process.env.PORT || 3030, async () => {
-  console.log("Server started at port 3030");
+const port = process.env.PORT || 3030;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
